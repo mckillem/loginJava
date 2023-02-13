95bf14a0632c74d0bf01bc890d0bfc40d4c14717
Matěj <mckillem@tuta.io>
2023-02-13 19:51:27 +0100

    app

---
+++ src/main/java/com/exercise/security/app/AppController.java
@@ -0,0 +1,11 @@
+package com.exercise.security.app;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/auth") //todo: what will be here?
+@RequiredArgsConstructor
+public class AppController {
+}
---
+++ src/main/java/com/exercise/security/app/AppService.java
@@ -0,0 +1,9 @@
+package com.exercise.security.app;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AppService {
+}
