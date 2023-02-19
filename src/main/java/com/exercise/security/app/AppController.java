a9723614f0d382381804eaf9b7a8889ceab269b1
Matěj <mckillem@tuta.io>
2023-02-19 23:37:51 +0100

    todos

--- src/main/java/com/exercise/security/app/AppController.java
+++ src/main/java/com/exercise/security/app/AppController.java
@@ -5,6 +5,10 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+// todo: How to secure it? An admin logs as an admin and the program recognises that it is an admin
+//  and then he can add applications and users. But how?
+//  Will he has a special page with users and applications which he can check or uncheck depending on what?
+// todo in auth.http
 @RequestMapping("/api/v1/app")
 @RequiredArgsConstructor
 public class AppController {
@@ -16,9 +20,9 @@ public class AppController {
 		return service.getApp(app);
 	}
 
-//	todo why it returns 403. Is it missing AppResponse? Is it because I need right rights to create a new app?
-//	I presume that only an administrator can manipulate applications so I need to create an administrator first
-//	How to add an app? Admin has to log in to add an app? How? Create a frontend
+//	todo: why it returns 403. Is it missing AppResponse? Is it because I need right rights to create a new app?
+//	 I presume that only an administrator can manipulate applications so I need to create an administrator first
+//	 How to add an app? Admin has to log in to add an app? How? Create a frontend
 	@PostMapping("/newApp")
 	@ResponseStatus(HttpStatus.OK)
 	public void createNewApp(@RequestBody AppRequest app) {
